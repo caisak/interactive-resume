66ca48122982a8841987ba2f454c6d4ccf641d30
Caisak <caisa.kohlin@gmail.com>
2022-11-30 22:13:04 +0100

    js read more read less button

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', main)
+
+function main() {
+    
+}
+
+function readMoreReadLess() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
